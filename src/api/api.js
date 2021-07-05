29db2fd8327b466aa8ee7443975ed7448775171f
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-05 09:53:24 +0300

    fixed api for deploy

--- src/api/api.js
+++ src/api/api.js
@@ -3,5 +3,5 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 export default axios.create({
-  baseURL: "https://milestep-test-task-backend.herokuapp.com/",
+  baseURL: "https://milestep-test-task-backend.herokuapp.com",
 });
