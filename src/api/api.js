f1e500ad7408822491b51ef09ebe7f52fac4e322
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-08 08:20:15 +0300

    fixed baseURL for backend

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 export default axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://milestep-test-task-backend.herokuapp.com",
 });
 
 // "http://localhost:3000"
