254ab81d7ca412db784f107b8a85066bcbb69ec7
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-07-29 23:30:28 +0300

    added static json fix routes

--- src/pages/profile/ProfileIcon.jsx
+++ src/pages/profile/ProfileIcon.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { routes } from "../../constants/routes";
-import { Link } from "react-router-dom";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import Tooltip from "@material-ui/core/Tooltip";
+import { NavLink } from "react-bootstrap";
 
 export const ProfileIcon = () => {
   return (
     <Tooltip title="Profile" placement="left">
-      <Link to={routes.PROFILE} className="profile__icon">
+      <NavLink to={routes.PROFILE} className="profile__icon">
         <AccountCircleIcon />
-      </Link>
+      </NavLink>
     </Tooltip>
   );
 };
